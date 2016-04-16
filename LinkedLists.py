e826e1276cd3f222c120ddb480f095462036fdcb
acurro <alberto.curro@cogecopeer1.com>
2016-04-16 01:51:58 +0100

    Renamed ArrayLinkedList.py to LinkedLists.py as the name reflects better the new schema. Finished implementation, code not tested yet!

--- LinkedLists.py
+++ LinkedLists.py
@@ -10,12 +10,24 @@ class Node(object):
 		self.__next = None
 
 	@property
+	def data(self):
+		return self.__data
+
+	@data.setter
+	def data(self, data=None):
+		self.__data = data
+
+	@property
 	def next(self):
 		return self.__next
 
 	@next.setter
 	def next(self, next=None):
 		self.__next = next
+
+	def __cmp__(self, other):
+		# Magic method for comparison
+		return self.__data == other.data
 
 
 """
@@ -38,73 +50,83 @@ class LinkedList(object):
 		self.__tail = node
 		self.__cnt += 1
 
+	def __find(self, data):
+		node = self.__head
+		prev = None
+		while node != None:
+			prev = node
+			node = node.next
+			if (node.data == data)
+				break
+
+		if node == None:
+			raise IndexError("Element was not found")
+		return prev, node
+
 	def delete(self, data):
 		"""
-		Deletes the given element
+		Deletes the given element.
+		:except: IndexError if no node is found with the given data on it
 		"""
-		node = self.__head
-		found = False
-		while node != None and not found:
-
-			self.__data.remove(data)
-		except ValueError:
-			# Convert into a IndexError
-			raise IndexError("Element was not found")
-		finally:
-			self.__data[-1].next = None
+		prev, node = self.__find(data)
+		prev.next = node.next
+		if node.next == None:
+			# move tail
+			self.__tail = prev
+		del node
+		self.__cnt -= 1
 
 	@property
 	def count(self):
-		return len(self.__data)
+		return self.__len__()
 
 	@property
 	def tail(self):
-	    return self.__data[-1]
+	    return self.__tail
 
 	@property
-	def head (self):
-	    return self.__data[0]
+	def head(self):
+	    return self.__head
 
 	# Set of magic methods supporting the standard pythonic ways of doing it
 	def __len__(self):
-		return len(self.__data)
+		return self.__cnt
 
 	def __getitem__(self, data):
-		return self.__data[data]
+		"""
+		:except: IndexError: no node containing given data could be found
+		"""
+		return self.__find(data)
 
 	def __delitem__(self, data):
+		"""
+		:except: IndexError: no node containing given data could be deleted
+		"""
 		self.delete(data)
 
 	def __iter__(self):
 		"""
 		This method adds "iterable" implementation support
 		"""
-		# iter(data) should not be necessary, since data is iterable itself!
-		return iter(data)
-
-	def __reversed__(self):
-		"""
-		Supporting reversed() call
-		"""
-		# Since the linked list is insertion-ordered, reversed() function can be supported
-		return iter(data.reversed())
+		return self
 
 	def __contains__(self, data):
 		"""
-		Supporting "in" call
+		Supporting "in" call (is 1 in list?)
 		"""
 		try:
-			self.__data.index(data)
-		except ValueError:
+			self.__find(data)
+		except IndexError:
 			return False
 		return True
 
 	def next(self):
 		"""
-		This method adds the iterator support (for i in x)
+		This method adds the iterator support (for each i in container x)
 		"""
-		for i in self.__data:
-			yield i
+		for (node = self.__head; node != self.__tail; node.next):
+			yield node
+		raise StopIteration
 
 
 """
@@ -166,12 +188,14 @@ class ArrayLinkedList(object):
 		# iter(data) should not be necessary, since data is iterable itself!
 		return iter(data)
 
-	def __reversed__(self):
-		"""
-		Supporting reversed() call
-		"""
-		# Since the linked list is insertion-ordered, reversed() function can be supported
-		return iter(data.reversed())
+	#def __reversed__(self):
+	#	"""
+	#	Supporting reversed() call
+	#	"""
+	# Tricky one! the internal array supports reversion, but the single linked list
+	# should not support it (next() only goes forward, never back, that needs a
+	# double linked list!). So this should not be present
+	#	return iter(data.reversed())
 
 	def __contains__(self, data):
 		"""
@@ -189,3 +213,4 @@ class ArrayLinkedList(object):
 		"""
 		for i in self.__data:
 			yield i
+		raise StopIteration
